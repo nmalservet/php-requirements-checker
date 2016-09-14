084a1b0c07bbaa97444c5d29a666f4763a6d3500
nicolas malservet <nicolas@malservet.eu>
2016-09-14 15:42:34 +0200

    add notes to install with composer

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ php-requirements-checker is a php script to integrate in each php software to ch
 
 ## How to use
 You need to copy the two files php-requirements-checker.php and my-requirements.php into your php application
+An other method will be to use composer to get the last stable release. Go to the composer section to do it.
+
+
 
 ## Add your own libraries, folders and file to check
 You need to add you rown libraries to check, and folders and file. To do it, edit the file my-requirements.php.
@@ -15,3 +18,23 @@ Via the web page : http://localhost/myapp/php-requirements-checker.php
 You will have a result in an html , like the example below 
 
 ![](https://cloud.githubusercontent.com/assets/7512899/18478975/8560e356-79d3-11e6-891c-cb3da42fed14.png)
+
+
+#Optional section : Install via Composer
+Composer is a powerfull tool o install external libraries. If you need to know how composer works, go on this page : https://getcomposer.org/
+
+
+To add this library to your installation via composer, you only need to add "nmalservet/php-requirements-checker" to your "require" section into your composer.json as example below :
+"require": {
+        "nmalservet/php-requirements-checker":"*",
+        ...
+    },
+
+Then, run the command "composer update".
+Composer will install the library into your "vendor" folder probably.
+
+You need to call the script from your installation. Open a file, for example my-checker.php then include the script
+require(__DIR__ . '/../vendor/nmalservet/php-requirements-checker/php-requirements-checker.php');
+
+So to check if your requirements are installed, you need to use this url:
+http://localhost/myapp/my-checker.php
--- php-requirements-checker.php
+++ php-requirements-checker.php
@@ -12,72 +12,72 @@
          * @return an array of the result ["TYPE","dependency","status","message","ERROR_CRITICALITY"]
          */
         function checkLibraries($libraries_required) {
-            $result = [];
-            foreach ($libraries_required as $library) {
-                $name = $library[1];
-                $version = $library[2];
-                $compatMode = $library[3];
-                $level = $library[4];
-                //check if the library exists
-                $status = "ko";
-                $message = "library not loaded";
-                //if name = PHP_VERSION
-                if ($name == "PHP_VERSION") {
-                    $cversion = phpversion();
-                    $comp = version_compare($version, $cversion);
-                    if ($comp == -1) {
-                        //case expected version < current version
-                        $status = "ok";
-                        $message = "PHP version " . $version . " expected, version " . $cversion . " founded";
-                    }
-                    if ($comp == 0) {
-                        $message = "PHP version " . $version . " expected is founded";
-                        $status = "ok";
-                    }
-                    if ($comp == 1) {
-                        $message = "PHP version " . $version . " expected, version " . $cversion . " founded";
-                        $status = "ko";
-                    }
-                } else
-                if (extension_loaded($name)) {
-                    $status = "ok";
-                    $message = "library loaded";
-                    //get version
-                    $cversion = phpversion($name);
-                    if ($version == $cversion) {
-                        $message.=", equal version ";
-                    } else {
-                        $comp = version_compare($version, $cversion);
-                        $message.=", version " . $version . " expected , version " . $cversion . " founded";
-                        if ($comp == -1) {
-                            //case expected version < current version
-                            if ($compatMode == "asc") {
-                                $status = "ko";
-                            }
-                            if ($compatMode == "desc") {
-                                $status = "ok";
-                            }
-                        }
-                        if ($comp == 0) {
-                            //cas version == cversion
-                            $message.=", equal version ";
-                        }
-                        if ($comp == 1) {
-                            //case expected version > current version
-                            if ($compatMode == "asc") {
-                                $status = "ok";
-                            }
-                            if ($compatMode == "desc") {
-                                $status = "ko";
-                            }
-                        }
-                    }
-                }
-                //check if the version is correct
-                $dependency = $name . " " . $version;
-                $result[] = ["LIBRARY", $dependency, $status, $message, $level];
-            }
-            return $result;
+        $result = [];
+        foreach ($libraries_required as $library) {
+        $name = $library[1];
+        $version = $library[2];
+        $compatMode = $library[3];
+        $level = $library[4];
+        //check if the library exists
+        $status = "ko";
+        $message = "library not loaded";
+        //if name = PHP_VERSION
+        if ($name == "PHP_VERSION") {
+        $cversion = phpversion();
+        $comp = version_compare($version, $cversion);
+        if ($comp == -1) {
+        //case expected version < current version
+        $status = "ok";
+        $message = "PHP version " . $version . " expected, version " . $cversion . " founded";
+        }
+        if ($comp == 0) {
+        $message = "PHP version " . $version . " expected is founded";
+        $status = "ok";
+        }
+        if ($comp == 1) {
+        $message = "PHP version " . $version . " expected, version " . $cversion . " founded";
+        $status = "ko";
+        }
+        } else
+        if (extension_loaded($name)) {
+        $status = "ok";
+        $message = "library loaded";
+        //get version
+        $cversion = phpversion($name);
+        if ($version == $cversion) {
+        $message.=", equal version ";
+        } else {
+        $comp = version_compare($version, $cversion);
+        $message.=", version " . $version . " expected , version " . $cversion . " founded";
+        if ($comp == -1) {
+        //case expected version < current version
+        if ($compatMode == "asc") {
+        $status = "ko";
+        }
+        if ($compatMode == "desc") {
+        $status = "ok";
+        }
+        }
+        if ($comp == 0) {
+        //cas version == cversion
+        $message.=", equal version ";
+        }
+        if ($comp == 1) {
+        //case expected version > current version
+        if ($compatMode == "asc") {
+        $status = "ok";
+        }
+        if ($compatMode == "desc") {
+        $status = "ko";
+        }
+        }
+        }
+        }
+        //check if the version is correct
+        $dependency = $name . " " . $version;
+        $result[] = ["LIBRARY", $dependency, $status, $message, $level];
+        }
+        return $result;
         }
 
         /**
@@ -89,31 +89,31 @@
          * @return an array of the result ["TYPE","FOLDER_NAME","status","message","ERROR_CRITICALITY"]
          */
         function checkFolders($folders_required) {
-            $result = [];
-            foreach ($folders_required as $folder) {
-                $name = $folder[0];
-                $access = $folder[1];
-                $level = $folder[2];
-                //check if the folder exists
-                //prepare the message
-                $status = "ko";
-                $message = "folder inexisting";
-                if (is_dir($name)) {
-                    $message = "folder exist";
-                    $status = "ok";
-                    //check rights
-                    if ($access == "w") {
-                        if (is_writable($name)) {
-                            $message.=", folder writable";
-                        } else {
-                            $status = "ko";
-                            $message.=", folder not writable";
-                        }
-                    }
-                }
-                $result[] = ["FOLDER", $name, $status, $message, $level];
-            }
-            return $result;
+        $result = [];
+        foreach ($folders_required as $folder) {
+        $name = $folder[0];
+        $access = $folder[1];
+        $level = $folder[2];
+        //check if the folder exists
+        //prepare the message
+        $status = "ko";
+        $message = "folder inexisting";
+        if (is_dir($name)) {
+        $message = "folder exist";
+        $status = "ok";
+        //check rights
+        if ($access == "w") {
+        if (is_writable($name)) {
+        $message.=", folder writable";
+        } else {
+        $status = "ko";
+        $message.=", folder not writable";
+        }
+        }
+        }
+        $result[] = ["FOLDER", $name, $status, $message, $level];
+        }
+        return $result;
         }
 
         /**
@@ -125,36 +125,36 @@
          * @return an array of the result ["TYPE","FOLDER_NAME","status","message","ERROR_CRITICALITY"]
          */
         function checkFiles($files_required) {
-            $result = [];
-            foreach ($files_required as $file) {
-                $name = $file[0];
-                $access = $file[1];
-                $level = $file[2];
-                $status = "ko";
-                $message = "file inexisting";
-                if (is_file($name)) {
-                    $message = "file exist";
-                    $status = "ok";
-                    if ($access == "w") {
-                        if (is_writable($name)) {
-                            $message.=", file writable";
-                        } else {
-                            $status = "ko";
-                            $message.=", file not writable";
-                        }
-                    }
-                    if ($access == "r") {
-                        if (is_readable($name)) {
-                            $message.=", file readable";
-                        } else {
-                            $status = "ko";
-                            $message.=", file not readable";
-                        }
-                    }
-                }
-                $result[] = ["FILE", $name, $status, $message, $level];
-            }
-            return $result;
+        $result = [];
+        foreach ($files_required as $file) {
+        $name = $file[0];
+        $access = $file[1];
+        $level = $file[2];
+        $status = "ko";
+        $message = "file inexisting";
+        if (is_file($name)) {
+        $message = "file exist";
+        $status = "ok";
+        if ($access == "w") {
+        if (is_writable($name)) {
+        $message.=", file writable";
+        } else {
+        $status = "ko";
+        $message.=", file not writable";
+        }
+        }
+        if ($access == "r") {
+        if (is_readable($name)) {
+        $message.=", file readable";
+        } else {
+        $status = "ko";
+        $message.=", file not readable";
+        }
+        }
+        }
+        $result[] = ["FILE", $name, $status, $message, $level];
+        }
+        return $result;
         }
 
         /**
@@ -162,18 +162,18 @@
          * @return an array of the result ["TYPE","dependency","status","message","ERROR_CRITICALITY"]
          */
         function checkAll($libraries_required, $folders_required, $files_required) {
-            $result = checkLibraries($libraries_required);
-            $result = array_merge($result, checkFolders($folders_required));
-            $result = array_merge($result, checkFiles($files_required));
-            return $result;
+        $result = checkLibraries($libraries_required);
+        $result = array_merge($result, checkFolders($folders_required));
+        $result = array_merge($result, checkFiles($files_required));
+        return $result;
         }
 
         /**
          * print the result into an html format
          */
         function toHTML($libraries_required, $folders_required, $files_required) {
-            $result = checkAll($libraries_required, $folders_required, $files_required);
-            $html = "<head>
+        $result = checkAll($libraries_required, $folders_required, $files_required);
+        $html = "<head>
                     <style>
                     :root{
                      --warn: #FFA07A;
@@ -187,23 +187,22 @@
 
                     </style>
                     </head><table stle=\"\">";
-            $html.="<tr><th>Status</th><th>Type</th><th>Name</th><th>Message</th></tr>";
-            foreach ($result as $line) {
-                $status = $line[2];
-                $level = $line[4];
-                $html.= "<tr><td style=\"" . ($status == "ok" ? "background-color:#90EE90;" : "background-color:var(--" . $level . ");") . "\">" . $status . "</td><td>" . $line[0] . "</td><td>" . $line[1] . "</td><td>" . $line[3] . "</td></tr></div>";
-            }
-            $html.="</table>";
-            return $html;
+        $html.="<tr><th>Status</th><th>Type</th><th>Name</th><th>Message</th></tr>";
+        foreach ($result as $line) {
+        $status = $line[2];
+        $level = $line[4];
+        $html.= "<tr><td style=\"" . ($status == "ok" ? "background-color:#90EE90;" : "background-color:var(--" . $level . ");") . "\">" . $status . "</td><td>" . $line[0] . "</td><td>" . $line[1] . "</td><td>" . $line[3] . "</td></tr></div>";
+        }
+        $html.="</table>";
+        return $html;
         }
 
         /**
          * print the result for a command-line call
          */
         function toString() {
-            
-        }
 
+        }
         echo toHTML($libraries_required, $folders_required, $files_required);
         ?>
 
